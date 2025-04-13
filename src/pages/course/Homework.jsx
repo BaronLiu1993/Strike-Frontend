79a52aea39980471f9f88b3e81ce9294840fe08a
BaronLiu <baronliu1993@gmail.com>
2025-04-13 18:23:58 -0400

    new capacitor build files

--- src/pages/course/Homework.jsx
+++ src/pages/course/Homework.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Typography, CircularProgress, Alert, Box } from "@mui/material";
 import { motion } from "framer-motion";
--- src/pages/create/AddComment.jsx
+++ src/pages/create/AddComment.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const AddComment = ({ submissionId }) => {
   const [commentText, setCommentText] = useState('');
